9a9c7d752e3e6cb63ca7b53a6ed7823f5d76c6fb
girinssh <tngus5851@naver.com>
2024-12-09 18:13:18 +0900

    implement Best Score
    Edit ioctl method

--- app/src/main/cpp/TextLCD.c
+++ app/src/main/cpp/TextLCD.c
@@ -8,11 +8,11 @@
 // Created by tngus on 2024-12-07.
 //
 
-#define PATH "/dev/score"
+#define PATH "/dev/Score"
 #define SCORE_MAGIC	0x04
 #define SCORE_RESET	_IOW(SCORE_MAGIC, 0, int)
 #define SCORE_UPDATE	_IOW(SCORE_MAGIC, 1, int)
-#define SCORE_RECORD	_IOW(SCORE_MAGIC, 2, int)
+#define BEST_SCORE_UPDATE _IOW(SCORE_MAGIC, 2, int)
 
 int getDeviceFile(){
     int fd = open(PATH, O_WRONLY);
@@ -31,7 +31,9 @@ Java_kr_ac_cau_1embedded_snakegame_MainActivity_sendScore2HW(JNIEnv *env, jobjec
         return;
     }
 
-    ioctl(fd, SCORE_UPDATE, (int)score, _IOC_SIZE(SCORE_UPDATE));
+    __android_log_print(ANDROID_LOG_INFO, "TEXTLCD_HW", "Score : %d", score);
+
+    ioctl(fd, SCORE_UPDATE, &score, _IOC_SIZE(SCORE_UPDATE));
     close(fd);
 }
 
@@ -44,5 +46,20 @@ Java_kr_ac_cau_1embedded_snakegame_MainActivity_resetLCD(JNIEnv *env, jobject th
     }
 
     ioctl(fd, SCORE_RESET, NULL);
+    close(fd);
+}
+
+JNIEXPORT void JNICALL
+Java_kr_ac_cau_1embedded_snakegame_MainActivity_sendBestScore2HW(JNIEnv *env, jobject thiz,
+                                                                 jint best_score) {
+    int fd = getDeviceFile();
+
+    if(fd < 0){
+        return;
+    }
+
+    __android_log_print(ANDROID_LOG_INFO, "TEXTLCD_HW", "Best : %d", best_score);
+
+    ioctl(fd, BEST_SCORE_UPDATE, &best_score, _IOC_SIZE(SCORE_UPDATE));
     close(fd);
 }
